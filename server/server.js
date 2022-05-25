2239b18db6a9af8161ac934e931a38296921025b
Valerie Bayard <valerie.bayard@gmail.com>
2022-05-25 12:05:00 +0100

    app route for get added and working

--- server/server.js
+++ server/server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
-const createRouter = require('./helpers/create_router');
-const cors = require('cors');
 
-app.use(cors())
-app.use(express.json())
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router');
+
+
+app.use(cors());
+app.use(express.json());
+
+MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
+    .then((client) => {
+    const db = client.db('hotel_bookings');
+    const bookingsCollection = db.collection('bookings');
+    const bookingsRouter = createRouter(bookingsCollection);
+    app.use('/api/bookings', bookingsRouter);
+    })
+    .catch(console.err);
+
 app.listen(9000, function(){
 console.log(`app listening on port ${this.address().port}`);
 })
